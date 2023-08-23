924ec14f98a27e8920fc44ced7e618b5a4fa22a4
Anas Alam <anasalam027@gmail.com>
2023-08-23 22:42:41 +0530

    changes

--- Routes/crop.js
+++ Routes/crop.js
@@ -3,7 +3,7 @@ const crop_controller = require("../Controllers/crop");
 const { verifyToken } = require("../Middlewares/user");
 
 router.post("/", crop_controller.get_crop);
-router.post("/", verifyToken, crop_controller.add_crop);
+router.post("/add_crop", verifyToken, crop_controller.add_crop);
 router.post("/edit_crop", verifyToken, crop_controller.edit_crop);
 router.delete("/", crop_controller.delete_crop);
 
