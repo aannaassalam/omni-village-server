5bc5664c1b439e8d8ad154719facf60137b7e68b
Anas Alam <anasalam027@gmail.com>
2024-01-13 22:45:38 +0530

    changes

--- Controllers/crop.js
+++ Controllers/crop.js
@@ -31,7 +31,7 @@ module.exports.get_crop = async (req, res) => {
           country: 1,
           status: 1,
           label: 1,
-          "label.name": "$label.name.en",
+          // "label.name": "$label.name.en",
         },
       },
     ]);
