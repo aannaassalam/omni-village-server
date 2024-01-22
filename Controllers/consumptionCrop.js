759175091f5c58d3393e90349c76c2418ac235fa
Anas Alam <anasalam027@gmail.com>
2024-01-23 01:42:17 +0530

    changes

--- Controllers/consumptionCrop.js
+++ Controllers/consumptionCrop.js
@@ -44,3 +44,34 @@ module.exports.get_consumption_crop = async (req, res) => {
     res.status(400).json(handleErrors(err));
   }
 };
+
+module.exports.get_consumption_crop_dashboard = async (req, res) => {
+  const { consumption_type_id } = req.params;
+  try {
+    const consumption_crops = await Promise.all([
+      await crop.find(
+        { label: consumption_type_id },
+        { name: `$name.en`, country: 1, status: 1, label: 1 }
+      ),
+      await treesCrop.find(
+        { label: consumption_type_id },
+        { name: `$name.${language}`, country: 1, status: 1, label: 1 }
+      ),
+      await poultryCrop.find(
+        { label: consumption_type_id },
+        { name: `$name.${language}`, country: 1, status: 1, label: 1 }
+      ),
+      await huntingCrop.find(
+        { label: consumption_type_id },
+        { name: `$name.${language}`, country: 1, status: 1, label: 1 }
+      ),
+      await fisheryCrop.find(
+        { label: consumption_type_id },
+        { name: `$name.${language}`, country: 1, status: 1, label: 1 }
+      ),
+    ]);
+    res.json(consumption_crops.flat());
+  } catch (err) {
+    res.status(400).json(handleErrors(err));
+  }
+};
