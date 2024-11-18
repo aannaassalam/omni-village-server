524299b28d10e9ceebab4069dd8df5cb50b4cfe9
Anas Alam <anasalam027@gmail.com>
2024-11-18 18:13:27 +0530

    other household and personal information changes

--- Routes/other-personal-household-items.js
+++ Routes/other-personal-household-items.js
@@ -1,26 +1,27 @@
 const router = require("express").Router();
 const { checkUser, verifyToken } = require("../Middlewares/user");
 const personal_household = require("../Controllers/other-personal-household-items");
+const ControllerWrapper = require("../utils/ControllerWrapper");
 
 router.get(
     "/",
     verifyToken,
     checkUser,
-    personal_household.get_personal_household_items
+    ControllerWrapper(personal_household.get_personal_household_items)
 );
 
 router.post(
     "/add-personal-household-items",
     verifyToken,
     checkUser,
-    personal_household.add_personal_household_items
+    ControllerWrapper(personal_household.add_personal_household_items)
 );
 
 router.put(
     "/edit-personal-household-items",
     verifyToken,
     checkUser,
-    personal_household.edit_personal_household_items
+    ControllerWrapper(personal_household.edit_personal_household_items)
 );
 
 module.exports = router;
