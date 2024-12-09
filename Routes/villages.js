87e4659608b04199ff2a47405e9787c545bac875
Anas Alam <anasalam027@gmail.com>
2024-12-09 13:11:09 +0530

    changes in villages api

--- Routes/villages.js
+++ Routes/villages.js
@@ -7,75 +7,15 @@ const {
 } = require("../Middlewares/user");
 const ControllerWrapper = require("../utils/ControllerWrapper");
 
-/**
- * @swagger
- * /villages/{country_name}:
- *    get:
- *      tags:
- *        - Villages
- *      summary: Get Villages.
- *      description: Get all Villages.
- *      produces:
- *        - application/json
- *      parameters:
- *        - name: country_name
- *          in: path
- *          description:
- *          schema:
- *              type: string
- *
- *
- *      responses:
- *        200:
- *          description: Successfully fetched data.
- *        400:
- *          description: Bad request
- *        500:
- *          description: Internal Server Error
- */
-router.get("/:country_name", checkUser, villages_controller.get_villages);
-router.get("/", villages_controller.get_all_villages);
 router.get(
     "/get-villages-for-moderator",
     verifyModeratorToken,
     checkModerator,
     ControllerWrapper(villages_controller.get_villages_for_moderator)
 );
+router.get("/:country_name", checkUser, villages_controller.get_villages);
+router.get("/", villages_controller.get_all_villages);
 
-/**
- * @swagger
- * /villages/add_village:
- *    post:
- *      tags:
- *        - Villages
- *      summary: Add Village.
- *      description: Add new Village.
- *      produces:
- *        - application/json
- *      parameters:
- *       - name: body
- *         in: body
- *         description:
- *         required: true
- *         schema:
- *           type: object
- *           properties:
- *             name:
- *               type: string
- *               example: Kolkata
- *             country:
- *               type: string
- *               example: India
- *
- *
- *      responses:
- *        200:
- *          description: Successfully fetched data.
- *        400:
- *          description: Bad request
- *        500:
- *          description: Internal Server Error
- */
 router.post("/add_village", checkUser, villages_controller.add_village);
 router.post("/edit_village", checkUser, villages_controller.edit_village);
 
